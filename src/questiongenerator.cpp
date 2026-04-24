f66f86ef333058aa0471c0e6d6df26f8663ca1c6
lucas-watkins <lucas-watkins@outlook.com>
2026-04-24 12:49:15 -0400

    Question Bugfix

--- src/questiongenerator.cpp
+++ src/questiongenerator.cpp
@@ -26,17 +26,25 @@ std::optional<QuestionGenerator::Question> QuestionGenerator::next() const {
             }
         }
 
+        std::uint16_t col_start{0};
+        for (std::uint16_t i{1}; i < sheet.columnCount(); ++i) {
+            if (auto cell{sheet.cell(1, i)}; cell.getString() == "Question") {
+                col_start = i;
+                break;
+            }
+        }
+
         std::uniform_int_distribution rows_possible{2U, question_cell};
 
         const std::uint32_t row{rows_possible(mt_engine)};
 
         return Question {
-            sheet.cell(row, 4).getString(),
-            sheet.cell(row, 5).getString(),
-            sheet.cell(row, 5).getString(),
-            sheet.cell(row, 6).getString(),
-            sheet.cell(row, 7).getString(),
-            sheet.cell(row, 8).getString(),
+            sheet.cell(row, col_start).getString(),
+            sheet.cell(row, col_start + 1).getString(),
+            sheet.cell(row, col_start + 1).getString(),
+            sheet.cell(row, col_start + 2).getString(),
+            sheet.cell(row, col_start + 3).getString(),
+            sheet.cell(row, col_start + 4).getString(),
         };
 
     } catch (const OpenXLSX::XLException& ex) {
